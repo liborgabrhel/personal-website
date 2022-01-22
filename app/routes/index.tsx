3e54a021e685b5dd5845c29f4c38f2a4b364ce16
Libor Gabrhel <gabrhel.libor@gmail.com>
2022-01-22 11:36:24 +0100

    feat: created web app with remix

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <main style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Personal Website</h1>
+      <p>This is my personal website.</p>
+    </main>
+  );
+}
